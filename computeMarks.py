41d5b3532c3f1055fcc35385ad8f2c8938e99b31
Prajwal Shenoy <prajwalkpshenoy@gmail.com>
2019-12-30 23:55:12 +0530

    Semesters 2,4,6,8 have been added... Problems with selective subject being excluded in semester 8 has been resolved

--- computeMarks.py
+++ computeMarks.py
@@ -4,9 +4,12 @@ import re
 import os
 import shutil
 import time
+import warnings
 from tabula import read_pdf
 from tabulate import tabulate
 from datetime import datetime
+
+warnings.filterwarnings("error")
 
 def computeMarksList(semester):
     startTime = time.time()
@@ -19,6 +22,68 @@ def computeMarksList(semester):
     conn = sq.connect(path)
     cur = conn.cursor()
     cur.execute('DROP TABLE IF EXISTS Marks')
+# -------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    if semester == '2':
+        cur.execute("""CREATE TABLE Marks (Name TEXT, USN TEXT, Sub1 TEXT, Sub2 TEXT, Sub3 TEXT,
+                     Sub4 TEXT, Sub5 TEXT, Sub6 TEXT, Sub7 TEXT, Sub8 TEXT, GPA TEXT)""")
+        for USN in USN_LIST:
+            USN = USN.rstrip()
+            try:
+                complete_table = read_pdf(os.path.abspath("Marks_list" + '/' + semester + "sem_marks" + '/' + USN + ".pdf"))
+                selective_table = complete_table.loc[1:, ['COURSE.1', 'GRADE', 'GRADE.1']]
+                table_to_list = selective_table.values.tolist()
+                read = PyPDF2.PdfFileReader(os.path.abspath("Marks_list" + '/' + semester + "sem_marks" + '/' + USN + ".pdf"))
+                page = read.getPage(0)
+                content = page.extractText()
+                student_name = re.findall("Student:(.*)USN", content)[0]
+                marks = []
+                for i in range(0, len(table_to_list)-1):
+                    marks.append(table_to_list[i][1])
+                cur.execute('INSERT INTO Marks (Name, USN) VALUES (?, ?)', (student_name, USN))
+                cur.execute('''UPDATE Marks SET Sub1 = ?, Sub2 = ?, Sub3 = ?, Sub4 = ?, Sub5 = ?, Sub6 = ?, Sub7 = ?, Sub8 = ?, GPA = ?  WHERE name = ?'''
+                            ,(marks[0], marks[1], marks[2], marks[3], marks[4], marks[5], marks[6], marks[7], table_to_list[len(table_to_list)-1][2], student_name))
+                success = success + 1
+                print(USN)
+            except:
+                print(USN + " did not get added")
+                faults = faults + 1
+                fault_list.append(USN)
+# -------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    if semester == '4':
+        cur.execute("""CREATE TABLE Marks (Name TEXT, USN TEXT, Sub1 TEXT, Sub2 TEXT, Sub3 TEXT,
+                     Sub4 TEXT, Sub5 TEXT, Sub6 TEXT, Sub7 TEXT, Sub8 TEXT, Sub9 TEXT, GPA TEXT)""")
+        for USN in USN_LIST:
+            USN = USN.rstrip()
+            try:
+                complete_table = read_pdf(os.path.abspath("Marks_list" + '/' + semester + "sem_marks" + '/' + USN + ".pdf"))
+                selective_table = complete_table.loc[1:, ['COURSE COURSE', 'GRADE', 'GRADE.1']]
+                table_to_list = selective_table.values.tolist()
+                read = PyPDF2.PdfFileReader(os.path.abspath("Marks_list" + '/' + semester + "sem_marks" + '/' + USN + ".pdf"))
+                page = read.getPage(0)
+                content = page.extractText()
+                student_name = re.findall("Student:(.*)USN", content)[0]
+                marks = []
+                for i in range(0, len(table_to_list)-1):
+                    if i != 7:
+                        marks.append(table_to_list[i][1])
+                cur.execute('INSERT INTO Marks (Name, USN) VALUES (?, ?)', (student_name, USN))
+                if len(marks) == 8:
+                    cur.execute('''UPDATE Marks SET Sub1 = ?, Sub2 = ?, Sub3 = ?, Sub4 = ?, Sub5 = ?, Sub6 = ?, Sub7 = ?, Sub8 = ?, Sub9 = ?, GPA = ?  WHERE name = ?'''
+                                ,(marks[0], marks[1], marks[2], marks[3], marks[4], marks[5], marks[6], marks[7], "NA", table_to_list[len(table_to_list)-1][2], student_name))
+                if len(marks) == 9:
+                    cur.execute('''UPDATE Marks SET Sub1 = ?, Sub2 = ?, Sub3 = ?, Sub4 = ?, Sub5 = ?, Sub6 = ?, Sub7 = ?, Sub8 = ?, Sub9 = ?, GPA = ?  WHERE name = ?'''
+                                ,(marks[0], marks[1], marks[2], marks[3], marks[4], marks[5], marks[6], marks[7], marks[8], table_to_list[len(table_to_list)-1][2], student_name))
+                success = success + 1
+                print(USN)
+            except FutureWarning:
+                print(USN + " did not get added")
+                faults = faults + 1
+                fault_list.append(USN)
+            except:
+                print(USN + " did not get added")
+                faults = faults + 1
+                fault_list.append(USN)
+# -------------------------------------------------------------------------------------------------------------------------------------------------------------------
     if semester == '6':
         cur.execute("""CREATE TABLE Marks (Name TEXT, USN TEXT, Sub1 TEXT, Sub2 TEXT, Sub3 TEXT,
                      Sub4 TEXT, Sub5 TEXT, Sub6 TEXT, Sub7 TEXT, Sub8 TEXT, GPA TEXT)""")
@@ -59,8 +124,13 @@ def computeMarksList(semester):
                 content = page.extractText()
                 student_name = re.findall("Student:(.*)USN", content)[0]
                 marks = []
-                for i in range(0, len(table_to_list)-1):
-                    marks.append(table_to_list[i][1])
+                if len(selective_table) > 7:
+                    for i in range(0, len(table_to_list)-1):
+                        if i != 2:
+                            marks.append(table_to_list[i][1])
+                else:
+                    for i in range(0, len(table_to_list)-1):
+                        marks.append(table_to_list[i][1])
                 # gpa = ((int(table_to_list[0][2])*3) + (int(table_to_list[1][2])*3) + (int(table_to_list[2][2])*15) + (int(table_to_list[3][2])*2) + (int(table_to_list[4][2])*2))/25
                 try:
                     gpa = table_to_list[len(table_to_list)-1][2]
