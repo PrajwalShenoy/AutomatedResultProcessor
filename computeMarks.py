05493a6529bbd738c8be2a9598487bebac303007
Prajwal Shenoy <prajwalkpshenoy@gmail.com>
2020-01-03 01:48:55 +0530

    Update with GUI working satisfactorily

--- computeMarks.py
+++ computeMarks.py
@@ -9,10 +9,20 @@ from tabula import read_pdf
 from tabulate import tabulate
 from datetime import datetime
 
+from PyQt5 import QtWidgets, uic, QtCore, QtGui
+
+# import front_page
+
 warnings.filterwarnings("error")
 
 def computeMarksList(semester):
     startTime = time.time()
+    USN_LIST = open(os.path.abspath("Student_list") + '/' + semester + "sem_students.txt", 'r')
+    numberOfLines = 0
+    for i in USN_LIST:
+        numberOfLines = numberOfLines + 1
+    forPer = 100/numberOfLines
+    print(numberOfLines)
     USN_LIST = open(os.path.abspath("Student_list") + '/' + semester + "sem_students.txt", 'r')
     faults = 0
     fault_list = []
@@ -26,7 +36,10 @@ def computeMarksList(semester):
     if semester == '2':
         cur.execute("""CREATE TABLE Marks (Name TEXT, USN TEXT, Sub1 TEXT, Sub2 TEXT, Sub3 TEXT,
                      Sub4 TEXT, Sub5 TEXT, Sub6 TEXT, Sub7 TEXT, Sub8 TEXT, GPA TEXT)""")
+        pres = 0
         for USN in USN_LIST:
+            pres = pres + forPer
+            call.progressBar.setValue(pres)
             USN = USN.rstrip()
             try:
                 complete_table = read_pdf(os.path.abspath("Marks_list" + '/' + semester + "sem_marks" + '/' + USN + ".pdf"))
@@ -48,11 +61,15 @@ def computeMarksList(semester):
                 print(USN + " did not get added")
                 faults = faults + 1
                 fault_list.append(USN)
+        call.progressBar.setValue(100)
 # -------------------------------------------------------------------------------------------------------------------------------------------------------------------
     if semester == '4':
         cur.execute("""CREATE TABLE Marks (Name TEXT, USN TEXT, Sub1 TEXT, Sub2 TEXT, Sub3 TEXT,
                      Sub4 TEXT, Sub5 TEXT, Sub6 TEXT, Sub7 TEXT, Sub8 TEXT, Sub9 TEXT, GPA TEXT)""")
+        pres = 0
         for USN in USN_LIST:
+            pres = pres + forPer
+            call.progressBar.setValue(pres)
             USN = USN.rstrip()
             try:
                 complete_table = read_pdf(os.path.abspath("Marks_list" + '/' + semester + "sem_marks" + '/' + USN + ".pdf"))
@@ -83,11 +100,15 @@ def computeMarksList(semester):
                 print(USN + " did not get added")
                 faults = faults + 1
                 fault_list.append(USN)
+        call.progressBar.setValue(100)
 # -------------------------------------------------------------------------------------------------------------------------------------------------------------------
     if semester == '6':
         cur.execute("""CREATE TABLE Marks (Name TEXT, USN TEXT, Sub1 TEXT, Sub2 TEXT, Sub3 TEXT,
                      Sub4 TEXT, Sub5 TEXT, Sub6 TEXT, Sub7 TEXT, Sub8 TEXT, GPA TEXT)""")
+        pres = 0
         for USN in USN_LIST:
+            pres = pres + forPer
+            call.progressBar.setValue(pres)
             USN = USN.rstrip()
             try:
                 complete_table = read_pdf(os.path.abspath("Marks_list" + '/' + semester + "sem_marks" + '/' + USN + ".pdf"))
@@ -109,11 +130,15 @@ def computeMarksList(semester):
                 print(USN + " did not get added")
                 faults = faults + 1
                 fault_list.append(USN)
+        call.progressBar.setValue(100)
 # -------------------------------------------------------------------------------------------------------------------------------------------------------------------
     if semester == '8':
+        pres = 0
         cur.execute("""CREATE TABLE Marks (Name TEXT, USN TEXT, Sub1 TEXT, Sub2 TEXT, Project TEXT,
                      TechinacalSeminar TEXT, Cred2 TEXT, GPA TEXT)""")
         for USN in USN_LIST:
+            pres = pres + forPer
+            call.progressBar.setValue(pres)
             USN = USN.rstrip()
             try:
                 complete_table = read_pdf(os.path.abspath("Marks_list" + '/' + semester + "sem_marks" + '/' + USN + ".pdf"))
@@ -140,22 +165,31 @@ def computeMarksList(semester):
                 cur.execute('''UPDATE Marks SET Sub1 = ?, Sub2 = ?, Project = ?, TechinacalSeminar = ?, Cred2 = ?, GPA = ?  WHERE name = ?'''
                             ,(marks[0], marks[1], marks[2], marks[3], marks[4], gpa, student_name))
                 success = success + 1
+                call.show()
                 print(USN)
+                # yield USN
             except:
                 print(USN + " did not get added")
                 faults = faults + 1
                 fault_list.append(USN)
+        call.progressBar.setValue(100)
 # --------------------------------------------------------------------------------------------------------------------------------------
     conn.commit()
     logFile = open("log_files.txt", 'a')
     logFile.write("--------------------------------------------------------" + '\n')
     logFile.write("processing of semester: " + semester + "\n")
+    call.terminal_display.append("processing of semester: " + semester)
     logFile.write(str(datetime.now())[:-7] + "\n")
+    call.terminal_display.append(str(datetime.now())[:-7])
     logFile.write("total number of success = " + str(success) + '\n')
+    call.terminal_display.append("total number of success = " + str(success))
     logFile.write("total number of faults = " + str(faults) + '\n')
+    call.terminal_display.append("total number of faults = " + str(faults))
     for i in fault_list:
         logFile.write(i + '\n')
+        call.terminal_display.append(i)
     logFile.write("Time taken: " + str(time.time() - startTime) + "seconds" + "\n")
+    call.terminal_display.append("Time taken: " + str(time.time() - startTime) + "seconds")
     logFile.close()
     print("--------------------------------------------------------")
     print("total number of success = " + str(success))
@@ -165,6 +199,22 @@ def computeMarksList(semester):
     print("--------------------------------------------------------")
 
 # --------------------------------------------------------------------------------------------------------------------------------------
+def getSemSpinBox():
+    semester_input = 0
+    semester_input = call.sem_select.value()
+    # call.terminal_display.append("Process initiated")
+    computeMarksList(str(semester_input))
+# inputSemester = input("Enter the semester to be processed: ")
+# computeMarksList(inputSemester)
+# front_page.executable()
+app = QtWidgets.QApplication([])
+call = uic.loadUi("front_page.ui")
 
-inputSemester = input("Enter the semester to be processed: ")
-computeMarksList(inputSemester)
+call.progressBar.setValue(0)
+
+# call.terminal_display.append("Program ready to start...")
+# call.process_pushButton.clicked.connect(lambda: computeMarksList(str(call.sem_spinBox.value())))
+call.process_pushButton.clicked.connect(lambda: getSemSpinBox())
+
+call.show()
+app.exec()
