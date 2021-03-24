27921dcce1a95e43302b20b58bf001d41cc0ef54
prajwalshenoy <prajwalkpshenoy@gmail.com>
2021-03-25 01:58:17 +0530

    figuring reg_ex for course code

--- webapp/get_marks.py
+++ webapp/get_marks.py
@@ -15,22 +15,28 @@ RESULT_URL = "https://www.dsce.edu.in/results"
 
 class HelloWorld(Resource):
     def get(self):
-        return {'hello': 'world'}
+        return {'Greeting': 'Hello There'}
 
-class uploadFile(Resource):
+class downloadResults(Resource):
     def post(self):
         try:
-            file = request.files['student_list']
-            if file and allowed_filename(file.filename):
-                filename = secure_filename(file.filename)
-                file.save(os.path.join(UPLOAD_FOLDER, filename))
-                failed_downlods = get_marks(filename)
-                return {'status': filename + ' saved successfully',
-                        'downloads': 'Complete',
-                        'failed_downloads': failed_downlods
-                        }
+            files_uploaded = receive_upload(request)
+            if files_uploaded[0]:
+                return get_marks(files_uploaded[1])
+            else:
+                return {'error': 'File could not be uploaded'}
         except:
             return {'error': 'Something is wrong'}
+
+def receive_upload(request):
+    try:
+        file = request.files['student_list']
+        if file and allowed_filename(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(UPLOAD_FOLDER, filename))
+        return True, filename
+    except:
+        return False, 'false'
 
 def allowed_filename(file_name):
     return file_name.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
@@ -54,10 +60,13 @@ def get_marks(student_file):
                     continue
                 with open(os.path.join(DOWNLOADS_FOLDER, student.rstrip().upper())+'.pdf', 'wb') as result_file:
                     result_file.write(result.content)
-    return failed_downloads
+    return {'status': filename + ' saved successfully',
+            'downloads': 'Complete',
+            'failed_downloads': failed_downlods
+            }
 
 api.add_resource(HelloWorld, '/', '/hello')
-api.add_resource(uploadFile, '/get-marks')
+api.add_resource(uploadFile, '/download-results')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
--- webapp/process_marks.py
+++ webapp/process_marks.py
@@ -1,11 +1,40 @@
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 from werkzeug.utils import secure_filename
+from get_marks import receive_upload, allowed_filename
+from get_marks import UPLOAD_FOLDER, DOWNLOADS_FOLDER, ALLOWED_EXTENSIONS
 import pandas as pd
 import os
 import requests
 import PyPDF2
 import re
+
+class veri(Resource):
+
+
+def verify_downloads(student_file):
+    file_list = os.listdir('downloads')
+    with open(os.path.join(UPLOAD_FOLDER, student_file)) as student_list:
+        for student in student_list:
+            if student.rstrip() + '.pdf' file_list:
+                continue
+            else:
+                return {'error': 'Some files are not present, please re-download them'}
+    return True
+
+def process_marks(student_file):
+    df = pd.DataFrame()
+    with open(os.path.join(UPLOAD_FOLDER, student_file)) as student_list:
+        for student in student_list:
+            pdf = PyPDF2.PdfFileReader(os.path.join(DOWNLOADS_FOLDER, student.rstrip()+'.pdf'))
+            num_of_pages = pdf.getNumPages()
+            for pg_num in range(num_of_pages):
+                process_page(pdf, pg_num, df)
+
+def process_page(pdf, pg_num, df):
+
+
+def check_and_columns():
 
 
 # regular expressions 
@@ -26,14 +55,25 @@ import re
 # df.loc['1DS18EC008', 'ENGINEERING MATHEMATICS II'] = 8
 # df.loc['1DS18EC008', 'ENGINEERING PHYSICS'] = 7
 
-# test = PyPDF2.PdfFileReader("/home/prajwal/Documents/Repositories/AutomatedResultProcessor/Marks_list/2sem_marks/1DS18EC008.pdf")
-# text = test.getPage(0).extractText()
+# pdf = PyPDF2.PdfFileReader("/home/prajwal/Documents/Repositories/AutomatedResultProcessor/Marks_list/2sem_marks/1DS18EC008.pdf")
+# text = pdf.getPage(0).extractText()
 # match = re.search(r"GRADE POINTS.*Letter Grades", text)
 # match.span()
 # (1119, 1495)
 # reduced_text = text[match.span()[0]:match.span()[1]]
+
+# Course Code
 # split_match = re.split(r"[0-9]{3}[A-Z]{2}[0-9][A-Z]{5}", reduced_text)
+# split_match = re.split(r"[A-Z]{2,3}[0-9]{2,3}", reduced_text)
+# split_match = re.split(r"[0-9]{2}[A-Z]{3,5}[0-9]{2,3}", reduced_text)
+# re.finditer(reg, reduced_text)
+# i.group() to get the match
+
 # split_match[-1], gpa = split_match[-1].split('SGPA')
 # split_match = split_match[1:]
 # re.split(r"[0-9]{1,2}", ex)
 # ['ENGINEERING MATHEMATICS Œ IV', 'S+', '']
+
+
+# os.listdir(os.path.abspath('downloads'))
+# ['file2', 'file1', 'file3']
