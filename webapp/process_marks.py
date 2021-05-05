7b87204f87598ae450a3321b7992036de22e379d
Prajwal Shenoy <prajwalkpshenoy@gmail.com>
2021-05-05 22:36:13 +0530

    Update process_marks.py

--- webapp/process_marks.py
+++ webapp/process_marks.py
@@ -111,78 +111,7 @@ def update_marks(split_match, df, student):
         df.loc[student, subject[0]] = subject[1]
     return df
 
-api.add_resource(ProcessMarks, '/process-marks')
+api.add_resource(ProcessMarks, '/')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=5001)
-
-# df = process_marks(student_file)
-# df.to_csv('report.csv')
-# for pg_num in range(num_of_pages):
-#     pdf_text = pdf.getPage(pg_num).extractText()
-
-#     match = re.search("(?<=GRADE POINTS1).*Letter Grades", pdf_text)
-#     reduced_text = pdf_text[match.span()[0]:match.span()[1]]
-#     lens = []
-#     for key, val in reg_ex.items():
-#             lens.append(len(re.split(val, reduced_text)[1:]))
-#     print(re.split(reg_ex[lens.index(max(lens))], reduced_text)[1:])
-
-# regular expressions 
-# "Student:.*USN"
-# "(?<=Student:).*USN"
-# "Roll No:.*Branch"
-# "GRADE POINTS.*Letter Grades"
-# "[0-9]{3}[A-Z]{2}[0-9][A-Z]{5}"
-
-# regex for splitting subject into marks
-# "[0-9]{1}[A-Z]{1}[+]{0,1}"
-
-# df['ENGINEERING MATHEMATICS II'] = None
-# df['ENGINEERING PHYSICS'] = None
-# df['BASIC ELECTRICAL ENGINEERING'] = None
-# df['ELEMENTS OF CIVIL ENGINEERING AND MECHANICS'] = None
-# df['ELEMENTS OF MECHANICAL ENGINEERING'] = None
-# df['ENGINEERING PHYSICS LABORATORY'] = None
-# df['WORKSHOP PRACTICE'] = None
-# df['LANGUAGE LABORATORY Œ II (ENGLISH)'] = None
-
-# df.loc['1DS18EC008', 'ENGINEERING MATHEMATICS II'] = 8
-# df.loc['1DS18EC008', 'ENGINEERING PHYSICS'] = 7
-
-# pdf = PyPDF2.PdfFileReader("/home/prajwal/Documents/Repositories/AutomatedResultProcessor/Marks_list/2sem_marks/1DS18EC008.pdf")
-# text = pdf.getPage(0).extractText()
-# match = re.search(r"GRADE POINTS.*Letter Grades", text)
-# match.span()
-# (1119, 1495)
-# reduced_text = text[match.span()[0]:match.span()[1]]
-
-# Course Code
-# split_match = re.split(r"[0-9]{3}[A-Z]{2}[0-9][A-Z]{5}", reduced_text)
-# split_match = re.split(r"[A-Z]{2,3}[0-9]{2,3}", reduced_text)
-# split_match = re.split(r"[0-9]{2}[A-Z]{3,5}[0-9]{2,3}", reduced_text)
-# re.finditer(reg, reduced_text)
-# i.group() to get the match
-
-# split_match[-1], gpa = split_match[-1].split('SGPA')
-# split_match = split_match[1:]
-# re.split(r"[0-9]{1,2}", ex)
-# ['ENGINEERING MATHEMATICS Œ IV', 'S+', '']
-
-
-# os.listdir(os.path.abspath('downloads'))
-# ['file2', 'file1', 'file3']
-
-# for i in range(pdf.getNumPages()):
-#     text = pdf.getPage(i).extractText()
-#     match = re.search("(?<=GRADE POINTS1).*Letter Grades", text)
-#     reduced_text = text[match.span()[0]:match.span()[1]]
-#     lens = []
-#     for key, val in reg_ex.items():
-#             lens.append(len(re.split(val, reduced_text)[1:]))
-#             # print(key,val)
-#             # print(re.findall(val, reduced_text), len(re.findall(val, reduced_text)))
-#             # print(re.split(val, reduced_text)[1:], len(re.split(val, reduced_text)[1:]))
-#     print(reg_ex[lens.index(max(lens))])
-#     print(re.split(reg_ex[lens.index(max(lens))], reduced_text)[1:])
-#     print("----------------")
